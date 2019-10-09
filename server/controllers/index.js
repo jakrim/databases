ed7080b6fbe06ce47fecfd92151f56f75f847830
Jesse Krim <jesse.krim@gmail.com>
2019-10-09 16:01:19 -0600

    Update Controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,39 +3,44 @@ var models = require('../models');
 module.exports = {
   messages: {
     get: function(req, res) {
-      // models.messages.get();
-      // console.log(res);
       models.messages.get((err, message) => {
         if (err) {
           res.sendStatus(500);
-          //     console.log('error: ', err);
         } else {
           res.send(message);
         }
       });
-      // models.messages.get().then(results => res.send(results)
-      // ex: callback(models.messages.get)?
-      // res.send
-    }, // a function which handles a get request for all messages
+    },
     post: function(req, res) {
-      // console.log('HERE THIS IS ', req.body);
       var message = req.body;
 
       models.messages.post(message, (err, data) => {
         if (err) {
           res.send(err);
         } else {
-          res.send(data);
+          res.sendStatus(201);
         }
       });
-      // res.status - set status
-    } // a function which handles posting a message to the database
+    }
   },
 
   users: {
-    // Ditto as above
-    get: function(req, res) {},
-    post: function(req, res) {}
+    get: function(req, res) {
+      models.users.get(function(err, results) {
+        if (err) {
+          res.send(err);
+        }
+        res.json(results);
+      });
+    },
+    post: function(req, res) {
+      var params = [req.body.username];
+      models.users.post(params, function(err, results) {
+        if (err) {
+          res.send(err);
+        }
+        res.sendStatus(201);
+      });
+    }
   }
 };
-// module.exports.messages.get();
