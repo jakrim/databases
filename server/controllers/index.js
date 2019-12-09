817cd181cb831aa9e7c982fae786b0a90cadc91a
Jesse Krim <jesse.krim@gmail.com>
2019-12-09 17:15:58 -0500

    Update Index.js

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,46 +1,36 @@
 var models = require('../models');
 
 module.exports = {
-  messages: {
-    get: function(req, res) {
-      models.messages.get((err, message) => {
-        if (err) {
-          res.sendStatus(500);
-        } else {
-          res.send(message);
-        }
-      });
-    },
-    post: function(req, res) {
-      var message = req.body;
 
-      models.messages.post(message, (err, data) => {
-        if (err) {
-          res.send(err);
-        } else {
+    messages: {
+      get: function (req, res) {
+        models.messages.get(function(err, results) {
+          if (err) { /* do something */ }
+          res.json(results);
+        });
+      },
+      post: function (req, res) {
+        var params = [req.body.message, req.body.username, req.body.roomname];
+        models.messages.post(params, function(err, results) {
+          if (err) { /* do something */ }
           res.sendStatus(201);
-        }
-      });
-    }
-  },
-
-  users: {
-    get: function(req, res) {
-      models.users.get(function(err, results) {
-        if (err) {
-          res.send(err);
-        }
-        res.json(results);
-      });
+        });
+      }
     },
-    post: function(req, res) {
-      var params = [req.body.username];
-      models.users.post(params, function(err, results) {
-        if (err) {
-          res.send(err);
-        }
-        res.sendStatus(201);
-      });
+
+    users: {
+      get: function (req, res) {
+        models.users.get(function(err, results) {
+          if (err) { /* do something */ }
+          res.json(results);
+        });
+      },
+      post: function (req, res) {
+        var params = [req.body.username];
+        models.users.post(params, function(err, results) {
+          if (err) { /* do something */ }
+          res.sendStatus(201);
+        });
+      }
     }
-  }
-};
+  };
